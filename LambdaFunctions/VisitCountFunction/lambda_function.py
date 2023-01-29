d9e1f0e16f84087d2acf9e32b08dd5ae9b45134c
root <root@MainPC.localdomain>
2023-01-28 21:45:09 -0800

    Lambda Functions

---
+++ LambdaFunctions/VisitCountFunction/lambda_function.py
@@ -0,0 +1,25 @@
+import boto3
+import os
+
+def lambda_handler(event: any, context: any):
+    user = "visitor"
+    visit_count: int=0
+
+    #Create a DynamoDB client
+    dynamodb = boto3.resource("dynamodb")
+    table_name = os.environ["TABLE_NAME"]
+    table = dynamodb.Table(table_name)
+
+    #Get the current visit count
+    response = table.get_item(TableName=table_name, Key={"user": user})
+    if "Item" in response:
+         visit_count = response["Item"]["count"]
+
+    # Increment the visit count
+    visit_count += 1
+
+    # Put the new visit count into the DynamoDB table
+    table.put_item(Item={"user": user, "count": visit_count})
+
+    counter = f"{visit_count}"
+    return {"message": counter}
