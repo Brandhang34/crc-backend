34d49034ff1741cedd7d13a35e9c4e843c38a7d6
Brandon Hang <brandonhang34@outlook.com>
2024-09-29 13:59:26 -0700

    Add state file to s3

--- Terraform/state.tf
+++ Terraform/state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "brandonhang.co-terraform-state-file"
-    key    = "portfolio-terraform-state-file"
+    key    = "portfolio-terraform-state-file/terraform.tfstate"
     region = "us-east-1"
   }
 }
